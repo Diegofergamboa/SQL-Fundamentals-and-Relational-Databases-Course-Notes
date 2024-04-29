c4a987f77ae04f8ac3213b6ae0c624969c71ae53
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-04-29 16:43:16 -0500

    Null review

---
+++ Statements/08_null.sql
@@ -0,0 +1,9 @@
+/*
+    Used in cases where we need
+    for example the emails are missing
+    and send a push for marketing
+*/
+
+SELECT * FROM users WHERE email IS NULL
+
+SELECT * FROM users WHERE email IS NOT NULL
