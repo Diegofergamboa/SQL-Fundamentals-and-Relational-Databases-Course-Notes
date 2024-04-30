6610700671703ebc9b2c31da7f17e09cb067dcdc
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-04-30 16:33:38 -0500

    Create and update reviewed

---
+++ Database/01_insert_in.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, surname) VALUES ('Paco', 'Perez')
+
+INSERT INTO users (users_id, name, surname) VALUES (8, 'Maria', 'Lopez')
---
+++ Database/02_update.sql
@@ -0,0 +1,6 @@
+-- Always use WHERE statement
+UPDATE users SET age = '21' WHERE users_id = 8
+
+UPDATE users SET age = '20' WHERE users_id = 9
+
+UPDATE users SET age = '20', init_date = '2020-03-03' WHERE users_id = 10
