5ce3f942019200e6257e0d24f4354ebc054a4d48
Diego Ortiz Gamboa <diegofergamboa@Diegos-MacBook-Pro.local>
2024-05-01 17:23:20 -0500

    Tables create drop and alter

--- Tables/01_create.sql
+++ Tables/01_create.sql
@@ -1,5 +1,4 @@
 -- Basic table creation
-
 CREATE TABLE persons (
 	id int,
     name varchar(45),
@@ -9,7 +8,6 @@ CREATE TABLE persons (
 );
 
 -- Restriction
-
 CREATE TABLE persons2 (
 	id int NOT NULL,
     name varchar(45) NOT NULL,
@@ -39,5 +37,44 @@ CREATE TABLE persons4 (
     created datetime,
     UNIQUE(id),
     PRIMARY KEY(id)
+);
+
+
+-- Primary Key and check restriction
+CREATE TABLE persons5 (
+	id int NOT NULL,
+    name varchar(45) NOT NULL,
+    age int,
+    email varchar(45),
+    created datetime,
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
+);
+
+
+-- Primary Key and default restriction
+CREATE TABLE persons6 (
+	id int NOT NULL,
+    name varchar(45) NOT NULL,
+    age int,
+    email varchar(45),
+    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
+);
+
+
+-- Primary Key and auto increment restriction
+CREATE TABLE persons7 (
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(45) NOT NULL,
+    age int,
+    email varchar(45),
+    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
 );
 
---
+++ Tables/03_alter_table.sql
@@ -0,0 +1,9 @@
+ALTER TABLE persons7
+
+ADD surname varchar(100);
+
+RENAME COLUMN surname TO descriptiona
+
+MODIFY COLUMN descriptiona varchar(150);
+
+DROP COLUMN description
